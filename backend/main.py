cf9e3240838b5a947b9b649684d07f2c8afed281
Azam <mazam1488@gmail.com>
2021-12-07 22:25:30 -0500

    add questions, submit and db

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Body
+import sqlite3
+import hashlib
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI()
+
+origins = ["http://0.0.0.0:8900"]
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class AuthRequest(BaseModel):
+    username: str
+    password: str
+
+def is_valid_user(request_username, request_password):
+    db_connection = sqlite3.connect('sqnx-db.db')
+    db_cursor = db_connection.cursor()
+    stored_hash = db_cursor.execute("SELECT password FROM User WHERE username = ?", [request_username]).fetchall()
+    if (hashlib.sha256(request_password.encode('utf-8')).hexdigest() == stored_hash[0][0]):
+        return True
+    else:
+        return False
+
+@app.post("/login")
+async def login(request: AuthRequest, username: str = Header(None), password: str = Header(None)):
+    if (is_valid_user(username, password)):
+        return request.username
+    else:
+        return "Invalid user"
+
+@app.post("/questions")
+async def fetch_questions(request: AuthRequest):
+    if (is_valid_user(request.username, request.password)):
+        db_connection = sqlite3.connect('sqnx-db.db')
+        db_connection.row_factory = sqlite3.Row
+        db_cursor = db_connection.cursor()
+        rows = db_cursor.execute("SELECT * FROM Questions").fetchall()
+        return json.dumps([dict(iterator) for iterator in rows])
+
+@app.post("/questions")
+async def fetch_questions(request: AuthRequest):
+    if (is_valid_user(request.username, request.password)):
+        db_connection = sqlite3.connect('sqnx-db.db')
+        db_connection.row_factory = sqlite3.Row
+        db_cursor = db_connection.cursor()
+        rows = db_cursor.execute("SELECT * FROM Questions").fetchall()
+        return json.dumps([dict(iterator) for iterator in rows])
+
+@app.post("/submit")
+async def login(request: dict = Body(...), username: str = Header(None), password: str = Header(None), number_of_questions: int = Header(None)):
+    if (is_valid_user(username, password)):
---
+++ ui/js/main.js
@@ -0,0 +1,106 @@
+function onLogin() {
+    var loginRequest = new XMLHttpRequest();
+    var inputUsername = document.getElementById("username").value;
+    var inputPassword = document.getElementById("password").value;
+    loginRequest.withCredentials = true;
+
+    loginRequest.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            var response = JSON.parse(this.responseText);
+            if (response == inputUsername) {
+                var sessionStorage = window.sessionStorage;
+                sessionStorage.setItem('authUsername', inputUsername);
+                sessionStorage.setItem('authPassword', inputPassword);
+                window.location.href = "questions.html";
+            } else {
+                alert("Invalid username or password");
+            }
+        }
+    });
+
+    loginRequest.open("POST", "http://0.0.0.0:8000/login");
+    loginRequest.setRequestHeader("Content-Type", "application/json");
+    loginRequest.setRequestHeader("username", inputUsername);
+    loginRequest.setRequestHeader("password", inputPassword);
+    var data = JSON.stringify({ username: inputUsername, password: inputPassword });
+    loginRequest.send(data);
+}
+
+function beginSurvey() {
+    var questionRequest = new XMLHttpRequest();
+    var sessionUsername = sessionStorage.getItem('authUsername');
+    var sessionPassword = sessionStorage.getItem('authPassword');
+    questionRequest.withCredentials = true;
+    document.getElementById("beginSurvey").style.display = "none";
+    document.getElementById("submiteSurvey").style.display = "block";
+
+    questionRequest.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            var jsonData = JSON.parse(JSON.parse(this.response));
+            var container = document.getElementById("questionContainer");
+            window.sessionStorage.setItem('numberOfQuestions', jsonData.length);
+            for (var index = 0; index < jsonData.length; index++) {
+                var question = jsonData[index];
+                container.innerHTML += '<div class="question-entry margin-bottom-big">'
+                + '<h6>'+ question["questionCategory"] +'</h6>'
+                + '<h7>'+ question["questionSub"] +'</h7>'
+                + '<h3>'+ question["questionText"] +'</h3>'
+                + '<select class="form-control margin" id="'+ question["questionId"] +'">'
+                + '<option>0</option>'
+                + ' <option>10</option>'
+                + ' <option>15</option>'
+                + ' <option>20</option>'
+                + ' <option>25</option>'
+                + ' <option>30</option>'
+                + ' <option>35</option>'
+                + ' <option>40</option>'
+                + ' <option>45</option>'
+                + ' <option>50</option>'
+                + ' <option>55</option>'
+                + ' <option>60</option>'
+                + ' <option>65</option>'
+                + ' <option>70</option>'
+                + ' <option>75</option>'
+                + ' <option>80</option>'
+                + ' <option>85</option>'
+                + ' <option>90</option>'
+                + ' <option>95</option>'
+                + ' <option>100</option>'
+                + '</select>'
+                + '</div>'
+            }
+        }
+    });
+
+    questionRequest.open("POST", "http://0.0.0.0:8000/questions");
+    questionRequest.setRequestHeader("Content-Type", "application/json");
+    var data = JSON.stringify({ username: sessionUsername, password: sessionPassword });
+    questionRequest.send(data);
+}
+
+function surveySubmit() {
+    var numberOfQuestions = window.sessionStorage.getItem('numberOfQuestions');
+    var inputData = {};
+    for (var index = 1; index <= numberOfQuestions; index++) {
+        inputData[index.toString()] = document.getElementById(index).value;
+    }
+    var surveySubmissionRequest = new XMLHttpRequest();
+    var sessionUsername = sessionStorage.getItem('authUsername');
+    var sessionPassword = sessionStorage.getItem('authPassword');
+    surveySubmissionRequest.withCredentials = true;
+
+    surveySubmissionRequest.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            alert(this.responseText);
+        }
+    });
+
+
+    surveySubmissionRequest.open("POST", "http://0.0.0.0:8000/submit");
+    surveySubmissionRequest.setRequestHeader("Content-Type", "application/json");
+    surveySubmissionRequest.setRequestHeader("username", sessionUsername);
+    surveySubmissionRequest.setRequestHeader("password", sessionPassword);
+    surveySubmissionRequest.setRequestHeader("number_of_questions", numberOfQuestions);
+    var data = JSON.stringify(inputData);
+    surveySubmissionRequest.send(data);
+}
